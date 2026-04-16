1e53254af7c69fbe66351b856f7a86c14ec81322
rxsxyy <rxxxy@protonmail.com>
2026-04-15 20:03:59 -0400

    fix:

--- src/main.c
+++ src/main.c
@@ -7,4 +7,6 @@ i32 main(i32 argc, char **argv) {
         // check for flags, parse and run if there are any.
         OptionFlag flags = parse_flags(argc, argv);
         run_flags(&flags);
+        
+        return 0;
 }
