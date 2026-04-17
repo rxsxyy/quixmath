b72adaa70277cd2b8e3f726b81b0ae4e2e74cd7b
rxsxyy <rxxxy@protonmail.com>
2026-04-16 20:20:14 -0400

    chore: cleaned formatting 4 spaces instead of 8 for indentation

--- src/flags.c
+++ src/flags.c
@@ -7,89 +7,89 @@
 #include "string.h"
 
 const Option OPTION_TABLE[] = {
-        // help options
-        {       "-h",   "prints this help message;", O_INT, offsetof(OptionFlag, msg), MK_HELP},
-        {   "--help",                            "", O_INT, offsetof(OptionFlag, msg), MK_HELP},
-        // version options
-        {       "-v", "prints version information;", O_INT, offsetof(OptionFlag, msg),  MK_VER},
-        {"--version",                            "", O_INT, offsetof(OptionFlag, msg),  MK_VER},
+    // help options
+    {       "-h",   "prints this help message;", O_INT, offsetof(OptionFlag, msg), MK_HELP},
+    {   "--help",                            "", O_INT, offsetof(OptionFlag, msg), MK_HELP},
+    // version options
+    {       "-v", "prints version information;", O_INT, offsetof(OptionFlag, msg),  MK_VER},
+    {"--version",                            "", O_INT, offsetof(OptionFlag, msg),  MK_VER},
 };
 
 const usize OPTION_TABLE_LEN = sizeof(OPTION_TABLE) / sizeof(OPTION_TABLE[0]);
 
 OptionFlag parse_flags(i32 argc, char **argv) {
-        OptionFlag flags = {NULL, NULL, NULL, MK_NONE, 0};
+    OptionFlag flags = {NULL, NULL, NULL, MK_NONE, 0};
 
-        for (i32 i = 1; i < argc; i++) {
-                bool matched = false;
+    for (i32 i = 1; i < argc; i++) {
+        bool matched = false;
 
-                for (usize j = 0; j < OPTION_TABLE_LEN; j++) {
-                        if (strcmp(argv[i], OPTION_TABLE[j].name) != 0)
-                                continue;
+        for (usize j = 0; j < OPTION_TABLE_LEN; j++) {
+            if (strcmp(argv[i], OPTION_TABLE[j].name) != 0)
+                continue;
 
-                        char *base = (char *)&flags;
+            char *base = (char *)&flags;
 
-                        switch (OPTION_TABLE[j].kind) {
-                        case O_BOOL:
-                                *(bool *)(base + OPTION_TABLE[j].offset) = (bool)OPTION_TABLE[j].value;
-                                break;
-                        case O_INT:
-                                if (OPTION_TABLE[j].offset == offsetof(OptionFlag, msg) && flags.msg != MK_NONE) {
-                                        fprintf(stderr, "error: unknown flag or extra argument: %s\n\n", argv[i]);
-                                        matched = true;
-                                        goto next; // don't be scared now
-                                }
-                                *(i32 *)(base + OPTION_TABLE[j].offset) = OPTION_TABLE[j].value;
-                                break;
-                        case O_STR:
-                                if (i + 1 < argc)
-                                        *(char **)(base + OPTION_TABLE[j].offset) = argv[++i];
-                                break;
-                        case O_NONE:
-                                break;
-                        }
-
-                        matched = true;
-                        break;
+            switch (OPTION_TABLE[j].kind) {
+            case O_BOOL:
+                *(bool *)(base + OPTION_TABLE[j].offset) = (bool)OPTION_TABLE[j].value;
+                break;
+            case O_INT:
+                if (OPTION_TABLE[j].offset == offsetof(OptionFlag, msg) && flags.msg != MK_NONE) {
+                    fprintf(stderr, "error: unknown flag or extra argument: %s\n\n", argv[i]);
+                    matched = true;
+                    goto next; // don't be scared now
                 }
-        next: // boo!
+                *(i32 *)(base + OPTION_TABLE[j].offset) = OPTION_TABLE[j].value;
+                break;
+            case O_STR:
+                if (i + 1 < argc)
+                    *(char **)(base + OPTION_TABLE[j].offset) = argv[++i];
+                break;
+            case O_NONE:
+                break;
+            }
 
-                if (!matched) {
-                        if (argv[i][0] != '-' && flags.equation == NULL) {
-                                flags.equation = argv[i];
-                        } else {
-                                fprintf(stderr, "error: unknown flag or extra argument: %s\n\n", argv[i]);
-                        }
-                }
+            matched = true;
+            break;
         }
+    next: // boo!
 
-        return flags;
+        if (!matched) {
+            if (argv[i][0] != '-' && flags.equation == NULL) {
+                flags.equation = argv[i];
+            } else {
+                fprintf(stderr, "error: unknown flag or extra argument: %s\n\n", argv[i]);
+            }
+        }
+    }
+
+    return flags;
 }
 
 void print_help() {
-        printf("usage: qm [options] <expression>\n\n");
-        printf("options:\n");
-        for (usize i = 0; i < OPTION_TABLE_LEN; i++) {
-                printf("  %-14s %s\n", OPTION_TABLE[i].name, OPTION_TABLE[i].desc);
-        }
+    printf("usage: qm [options] <expression>\n\n");
+    printf("options:\n");
+    for (usize i = 0; i < OPTION_TABLE_LEN; i++) {
+        printf("  %-14s %s\n", OPTION_TABLE[i].name, OPTION_TABLE[i].desc);
+    }
 }
 
 void print_ver() {
-        printf("%s %s \n", PROGRAM, VERSION);
+    printf("%s %s \n", PROGRAM, VERSION);
 }
 
 void run_flags(const OptionFlag *flags) {
-        switch (flags->msg) {
-        case MK_ERR:
-                break;
-        case MK_VER:
-                print_ver();
-                break;
-        case MK_HELP:
-                print_help();
-                break;
-        case MK_NONE:
-                print_help();
-                break;
-        }
+    switch (flags->msg) {
+    case MK_ERR:
+        break;
+    case MK_VER:
+        print_ver();
+        break;
+    case MK_HELP:
+        print_help();
+        break;
+    case MK_NONE:
+        print_help();
+        break;
+    }
 }
--- src/main.c
+++ src/main.c
@@ -4,9 +4,17 @@
 #include "flags.h"
 
 i32 main(i32 argc, char **argv) {
-        // check for flags, parse and run if there are any.
-        OptionFlag flags = parse_flags(argc, argv);
-        run_flags(&flags);
-        
-        return 0;
+    // check for flags, parse and run if there are any.
+    OptionFlag flags = parse_flags(argc, argv);
+    run_flags(&flags);
+
+    // find equation, parse and evaluate.
+    // Node equation = parse_eq(flags.equation);
+    // f64 result = eval_eq(equation);
+    // free_node(equation);
+    
+    // print result
+    // print_result(result);
+
+    return 0;
 }
